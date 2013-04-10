aea4c33c8069b9f629bd15539297174db6931eca
will <will@will-TravelMate-2300.(none)>
2013-04-10 22:41:49 +0200

    un peu de code

--- src/util.c
+++ src/util.c
@@ -1,3 +1,16 @@
 
 #include <stdint.h>
 
+
+// Prend un entier sur 64 bit et retourne le (r,g,b)
+void getColor(uint64_t const *pixel, uint32_t *r,uint32_t *g,uint32_t *b) {
+	int decalr = 0;
+	int decalg = 0;
+	int decalb = 0;
+	int mask = 0;
+	
+	*r = (*pixel >> decalr /*decalage*/ ) & mask;
+	*g = (*pixel >> decalg /*decalage*/ ) & mask;
+	*b = (*pixel >> decalb /*decalage*/ ) & mask;
+
+}
